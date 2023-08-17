7928d4a9e7aa361b0a664504760f68e2def5b42c
KimSeoYeon <115128505+KimSeoYeon23@users.noreply.github.com>
2023-08-17 19:05:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -52,10 +52,10 @@ markdown_text = """
 <br/>
 <br/>
 
-![SeoYeons's GitHub stats](https://github-readme-stats.vercel.app/api?username=KimSeoYeon23&show_icons=true&theme=dark)
+![SeoYeons's GitHub stats](https://github-readme-stats-git-master-kimseoyeon23.vercel.app/api?username=KimSeoYeon23&show_icons=true&theme=github_dark)
 <br/>
 <br/>
-[![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=KimSeoYeon23&&theme=onedark&exclude_repo=github-readme-stats,anuraghazra.github.io)](https://github.com/KimSeoYeon23)
+[![Top Langs](https://github-readme-stats-git-master-kimseoyeon23.vercel.app/api/top-langs/?username=KimSeoYeon23&theme=github_dark&exclude_repo=github-readme-stats,anuraghazra.github.io)](https://github.com/KimSeoYeon23)
 <br/>
 <br/>
 [![Solved.ac Profile](http://mazassumnida.wtf/api/generate_badge?boj=kim7510)](https://solved.ac/kim7510)
